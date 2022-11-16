28eab50a0a9c09734c307e11f92c433b965d6ff9
prabath1998 <prabhathudayanga1998@gmail.com>
2022-11-16 13:17:56 +0530

    auth user can see the buy button tested

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,9 @@
 
     @forelse ($products as $product)
         <h2 class="font-bold">{{ $product->name }} - {{ $product->type }}</h2>
+        @auth
+            <button>Buy Product</button>
+        @endauth
 
     @empty
         <p>No products available</p>
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,12 +3,23 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
+
+    private $product;
+
+    public function setup(): void
+    {
+        parent::setup();
+        $this->product =
+            Product::factory()->create();
+    }
+
     public function test_products_route_return_ok()
     {
         $respose = $this->get('/products');
@@ -18,14 +29,23 @@ class ProductTest extends TestCase
 
     public function test_product_has_name()
     {
-        $product  = Product::factory()->create();
-        $this->assertNotEmpty($product->name);
+
+        $this->assertNotEmpty($this->product->name);
     }
 
     public function test_products_are_not_empty()
     {
+        $product = Product::factory()->create();
         $response = $this->get('/products');
-        $response->assertDontSee('No products available');
-        // $response->assertSee($product->name);
+        // $response->assertDontSee('No products available');
+        $response->assertSee($this->product->name);
+    }
+
+    public function test_auth_user_can_see_the_buy_button()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/products');
+
+        $response->assertSee('Buy Product');
     }
 }
