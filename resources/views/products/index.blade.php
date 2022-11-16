f57a67839c3dba463a9233989958088b848ccfc1
prabath1998 <prabhathudayanga1998@gmail.com>
2022-11-16 13:00:41 +0530

    products are empty tested

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,12 @@
 <x-guest-layout>
     <h1>products</h1>
+
+    @forelse ($products as $product)
+        <h2 class="font-bold">{{ $product->name }} - {{ $product->type }}</h2>
+
+    @empty
+        <p>No products available</p>
+    @endforelse ($products as $product)
+
+
 </x-guest-layout>
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,5 +14,17 @@ class ProductTest extends TestCase
         $respose = $this->get('/products');
         $respose->assertSee('products');
         $respose->assertStatus(200);
+    }
+
+    // public function test_product_has_name()
+    // {
+    //     $product  = Product::factory()->create();
+    //     $this->assertNotEmpty($product->name);
+    // }
+
+    public function test_products_are_empty()
+    {
+        $response = $this->get('/products');
+        $response->assertSee('No products available');
     }
 }
