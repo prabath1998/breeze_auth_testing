b4cf75cb88ec1e0c8a7eaf12df1176cb6d482f9b
prabath1998 <prabhathudayanga1998@gmail.com>
2022-11-16 13:04:45 +0530

    products are not empty

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -16,15 +16,16 @@ class ProductTest extends TestCase
         $respose->assertStatus(200);
     }
 
-    // public function test_product_has_name()
-    // {
-    //     $product  = Product::factory()->create();
-    //     $this->assertNotEmpty($product->name);
-    // }
+    public function test_product_has_name()
+    {
+        $product  = Product::factory()->create();
+        $this->assertNotEmpty($product->name);
+    }
 
-    public function test_products_are_empty()
+    public function test_products_are_not_empty()
     {
         $response = $this->get('/products');
-        $response->assertSee('No products available');
+        $response->assertDontSee('No products available');
+        // $response->assertSee($product->name);
     }
 }
