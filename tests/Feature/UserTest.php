99f727f06cdc808caa0a1c1f21ed7d6caf7458c6
prabath1998 <prabhathudayanga1998@gmail.com>
2022-11-16 11:21:39 +0530

    auth user can access dashboard tested

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -24,4 +24,11 @@ class UserTest extends TestCase
     $response->assertStatus(302);
     $response->assertRedirect('/dashboard');
   }
+
+  public function test_auth_user_can_access_dashboard()
+  {
+    $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/dashboard');
+    $response->assertStatus(200);
+  }
 }
